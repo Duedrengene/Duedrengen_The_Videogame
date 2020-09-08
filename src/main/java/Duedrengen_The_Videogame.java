672d6f554f00629add125dd5edaae3a0c3f7b183
Michael30502 <54103011+Michael30502@users.noreply.github.com>
2020-09-08 14:07:20 +0200

    Maven

---
+++ src/main/java/Duedrengen_The_Videogame.java
@@ -0,0 +1,7 @@
+import processing.core.PApplet;
+
+public class Duedrengen_The_Videogame {
+    public static void main(String[] args ) {
+        PApplet.main("Duedrengen_The_Videogame");
+    }
+}
