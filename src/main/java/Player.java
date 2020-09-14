c4936479bba3c277ec8232e6b781bc0f485fa56e
mathias124 <54176496+mathias124@users.noreply.github.com>
2020-09-14 11:12:00 +0200

    #16
    Opgraderet

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -8,45 +8,76 @@ public class Player {
     PFont Titelfont;
     PFont Lilletitelfont;
 
-        boolean isLeft, isRight, isUp, isDown;
-        int x, y;
-        int d, v;
-        int Level;
-        ImageLoader iL;
-        PApplet p;
+    boolean isLeft, isRight, isUp, isDown;
+    int x, y;
+    int d, v;
+    int Level;
+    float xPosition = 210, yPosition = 250;
+    float hojdetekst = 25, breddetekst = 250;
+    boolean klikket;
 
 
-        Player(int xx, int yy, int dd, int vv,ImageLoader iL,int Level,PApplet p,PFont Titelfont,PFont lilletitelfont) {
-            x = xx;
-            y = yy;
-            d = dd;
-            v = vv;
-            this.iL=iL;
-            this.Level=Level;
-            this.p = p;
-            this.Titelfont = Titelfont;
-            this.Lilletitelfont = lilletitelfont;
-        }
+    ImageLoader iL;
+    PApplet p;
 
 
+    Player(int xx, int yy, int dd, int vv, ImageLoader iL, int Level, PApplet p, PFont Titelfont, PFont lilletitelfont) {
+        x = xx;
+        //Hastighed i x retning.
+        y = yy;
+        //Hastighed i y retning.
+        d = dd;
+        //d = Størrelse af player (højde og bredde)
+        v = vv;
+        //v er med til at forme grænsen af vinduet i form af constrain.
+        this.iL = iL;
+        this.Level = Level;
+        this.p = p;
+        this.Titelfont = Titelfont;
+        this.Lilletitelfont = lilletitelfont;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
 
 
-        void simulate() {
-            if(Level ==0) {
-                p.image(iL.startup,0,0);
+    void simulate() {
+        if (Level == 0) {
+            p.image(iL.startup, 0, 0);
 
-                p.fill(220,20,60);
-                p.textAlign(p.CENTER);
-                p.textFont(Titelfont);
-                p.text("Velkommen tilbage Agent Duedreng!", 960, 85);
-                p.fill(0);
-                p.textFont(Lilletitelfont);
-                p.text("Tryk på hvilken som helst tast for at starte spillet",960,880);
-                if(p.keyPressed == true) {
-                    Level =1;
-                }
+            p.fill(220, 20, 60);
+            p.textAlign(p.CENTER);
+            p.textFont(Titelfont);
+            p.text("Velkommen tilbage Agent Duedreng!", 960, 85);
+            p.fill(0);
+            p.textFont(Lilletitelfont);
+            //p.text("Tryk på hvilken som helst tast for at starte spillet",960,880);
+
+
+            p.textSize(56);
+ p.fill(253,106,2);
+            p.text("Play", 210, 225);
+            p.text("Settings", 210, 400);
+            p.text("Quit", 210, 575);
+            if (p.keyPressed == true) {
+                Level = 1;
             }
         }
+    }
+
+    void registrerKlikket() {
+        if (p.mousePressed && p.mouseX < xPosition + breddetekst && p.mouseX > xPosition && p.mouseY < yPosition && p.mouseY < yPosition + hojdetekst) {
+            klikket = true;
+        } else {
+            klikket = false;
+
+        }
+    }
+
+    boolean erKlikket() {
+        return klikket;
+    }
+
+
         void display() {
             //ellipse(x, y, d, d);
             if(Level ==1){
