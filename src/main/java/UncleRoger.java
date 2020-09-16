32793a62e4703ff285061c7282126062611e037a
mathias124 <54176496+mathias124@users.noreply.github.com>
2020-09-16 10:07:27 +0200

    #5 Make Shop user friendly

--- src/main/java/UncleRoger.java
+++ src/main/java/UncleRoger.java
@@ -2,6 +2,8 @@ import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PVector;
 
+import javax.swing.*;
+
 public class UncleRoger {
     PApplet p;
     PVector position = new PVector();
@@ -44,19 +46,31 @@ void drawshop(Player player,int i) {
     p.rectMode(p.CORNER);
     if ((level == 1 && i==0 && player.interact==true && someoneIsClose)||p1shopOpen == true) {
         p.fill(255,108,0);
-        p.rect(100, 200, (p.width - 400)/2, p.height - 400);
+        p.rect(100, 200, (p.width - 900)/2, p.height - 400);
+        //Uncommon Item
         p.fill(108,255,0);
         p.rect(140,340,64,64);
         p.image(unclePic.havregryn,140,340);
         p.rect(490,340,64,64);
         p.image(unclePic.friedrice,490,350 );
-        p.rect(140,540,64,64);
-        p.image(unclePic.majs,140,540);
         p.rect(490,540,64,64);
         p.image(unclePic.msgbull,490,540);
         p.rect(140,740,64,64);
         p.image(unclePic.Syringe,140,740);
         p.rect(490,740,64,64);
+        p.rect(267,245,20,20);
+        p.fill(0);
+        p.textSize(18);
+        p.text("Uncommen item: ",200,260);
+        //Ability Player 1
+        p.text("Ability:",480,260);
+        p.fill(63,72,204);
+        p.rect(507,245,20,20);
+        p.rect(140,540,64,64);
+        p.image(unclePic.majs,140,540);
+
+
+
         p1shopOpen = true;
     }
     if ((level == 1  && i==1 && player.interact==true && someoneIsClose)||p2shopOpen == true) {
