c28bda527b44d5c661c3b71d342ae0699e871a3f
mathias124 <54176496+mathias124@users.noreply.github.com>
2020-09-16 08:41:10 +0200

    #7 Update picture
    Update the item list in Uncle rogers shop

--- src/main/java/ImageLoader.java
+++ src/main/java/ImageLoader.java
@@ -25,9 +25,26 @@ void loadTheImages(int var,int w,int h){
 
     scaleW = (float)w/1920;
     scaleH = (float)h/1080;
+
     img = p.loadImage("duedreng.png");
     startup = p.loadImage("pigeon.png");
     uncleRoger = p.loadImage("Uncle Roger.png");
+    //items.
+    msgbull = p.loadImage("msgbull.png");
+    msg = p.loadImage("MSG salt on crack.png");
+    majs = p.loadImage("majs.png");
+    friedrice = p.loadImage("Fried rice (ikke jamie edition).png");
+    Syringe = p.loadImage("Syringe stor.png");
+    havregryn = p.loadImage("havregryn.png");
+// Resolution resize.
+    
+    msg.resize((int)(64*scaleW),(int)(64*scaleH));
+    msgbull.resize((int)(64*scaleW),(int)(64*scaleH));
+    havregryn.resize((int)(64*scaleW),(int)(64*scaleH));
+    friedrice.resize((int)(64*scaleW),(int)(64*scaleH));
+    Syringe.resize((int)(64*scaleW),(int)(64*scaleH));
+    majs.resize((int)(64*scaleW),(int)(64*scaleH));
+    img.resize((int)(64*scaleW),(int)(64*scaleH));
     startup.resize(w,h);
     img.resize((int)(64*scaleW),(int)(64*scaleH));
     uncleRoger.resize((int)(64*scaleW),(int)(64*scaleH));
