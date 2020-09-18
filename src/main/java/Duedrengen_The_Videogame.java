51fa6a36fc2e807db14a325df6e5f47b1a377a5b
Marco Roland <69676695+iopoli0@users.noreply.github.com>
2020-09-18 16:41:59 +0200

    Memes

--- src/main/java/Duedrengen_The_Videogame.java
+++ src/main/java/Duedrengen_The_Videogame.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Duedrengen_The_Videogame extends PApplet {
 
-boolean pressed = false;
+    boolean pressed = false;
     int speed = 4;
     int level = 0;
     int dueAmount = 2;
@@ -19,7 +19,7 @@ boolean pressed = false;
     boolean mPressed = false;
     boolean settings = false;
 
-    PVector gravity = new PVector(0,(float)0.10);
+    PVector gravity = new PVector(0, (float) 0.10);
     Character[] p;
     ImageLoader imgLoad = new ImageLoader(this);
     ImageResizer imgResize = new ImageResizer(this, width, height, imgLoad);
@@ -27,7 +27,7 @@ boolean pressed = false;
     FontLoader fontLoad = new FontLoader(this);
     UncleRoger uncleroger;
     Backgrounds backgrounds = new Backgrounds(level, this, imgLoad, fontLoad, imgResize);
-    SoundLoader soundLoad = new SoundLoader(this,backgrounds);
+    SoundLoader soundLoad = new SoundLoader(this, backgrounds);
     ArrayList<Enemy> enemyList = new ArrayList<>();
     Button bStart = new Button(this, 210, 225 - 10, 1, backgrounds, imgResize);
     Button bSettings = new Button(this, 210, 390, 2, backgrounds, imgResize);
@@ -59,28 +59,27 @@ boolean pressed = false;
     }
 
 
-
-
     public void draw() {
 
         //soundLoad.playSounds();
         //background(0,255,0);
         background(53, 101, 77);
 
+        //Uncleroger and his background + player constuctor and enemy constuctor
         fill(220, 20, 60);
         textAlign(CENTER);
         textFont(fontLoad.titelFont);
         textSize(84 * imgResize.scaleW);
-
-        if(uncleroger ==null && backgrounds.level%2==0 &&backgrounds.level!= 0)
-            uncleroger = new UncleRoger(this, imgLoad, imgResize.width / 2 , imgResize.height/2, level, imgResize);
+        if (uncleroger == null && backgrounds.level % 2 == 0 && backgrounds.level != 0)
+            uncleroger = new UncleRoger(this, imgLoad, imgResize.width / 2, imgResize.height / 2, level, imgResize);
         if (backgrounds.simulate()) {
             for (int i = 0; i < dueAmount; i++) {
-                p[i] = new Character((int) random(0, 1920), (int) random(0, 1080), speed, imgLoad, level, this, imgResize, i,backgrounds,enemyList,gravity);
+                p[i] = new Character((int) random(0, 1920), (int) random(0, 1080), speed, imgLoad, level, this, imgResize, i, backgrounds, enemyList, gravity);
             }
             enemyList.add(new Enemy(200 * imgResize.scaleW, 984 * imgResize.scaleH, -2 * imgResize.scaleW, imgLoad, this, 1));
 
         }
+        //Background deathscreen
         if (p[0] != null)
             for (int i = 0; i < dueAmount; i++) {
                 if (!gameOver) {
@@ -91,7 +90,8 @@ boolean pressed = false;
                 }
             }
 
-                textFont(fontLoad.smallTitelFont);
+        //Buttons
+        textFont(fontLoad.smallTitelFont);
         textSize(56 * imgResize.scaleW);
         fill(253, 106, 2);
         //Play
@@ -105,36 +105,38 @@ boolean pressed = false;
         bQuit.draw(level, settings, gameOver);
         fill(0, 0, 0);
 
+        //Unclerogers shop
         if (!gameOver)
             if (p[0] != null) {
-                if(uncleroger!= null)
-                for (int i = 0; i < dueAmount; i++) {
-                    uncleroger.detectCharacter(p[i]);
-                    uncleroger.draw(backgrounds.level);
-                    uncleroger.lukShop(p[i]);
-                    uncleroger.drawshop(p[i], i);
-                    uncleroger.buyitem(p[i],i,backgrounds);
-                }
+                if (uncleroger != null)
+                    for (int i = 0; i < dueAmount; i++) {
+                        uncleroger.detectCharacter(p[i]);
+                        uncleroger.draw(backgrounds.level);
+                        uncleroger.lukShop(p[i]);
+                        uncleroger.drawshop(p[i], i);
+                        uncleroger.buyitem(p[i], i, backgrounds);
+                    }
 
-int var =p[0].levelTransition(pressed,oatList);
+                //level transition
+                int var = p[0].levelTransition(pressed, oatList);
 
-                    if(var !=0)
-                    p[1].location.x=0;
-                    if(var ==2)
-                        uncleroger= null;
+                if (var != 0)
+                    p[1].location.x = 0;
+                if (var == 2)
+                    uncleroger = null;
 
 
-
+                //Due and duedreng attack configurations
                 for (int i = 0; i < dueAmount; i++) {
                     p[i].iHaveShot = false;
                     p[i].move();
-                    if (p[i].shoot == true && oatList.size()<=p[i].shotAmount) {
+                    if (p[i].shoot == true && oatList.size() <= p[i].shotAmount) {
 
-                        oatList.add(new Oatmeal(this, p[i].location.x, p[i].location.y,imgLoad,imgResize, backgrounds));
-                        if(i==0)
-                        image(imgLoad.duedrenganimation, p[i].location.x, p[i].location.y);
-                        if(i==1)
-                         image(imgLoad.dueShitting, p[i].location.x, p[i].location.y);
+                        oatList.add(new Oatmeal(this, p[i].location.x, p[i].location.y, imgLoad, imgResize, backgrounds));
+                        if (i == 0)
+                            image(imgLoad.duedrenganimation, p[i].location.x, p[i].location.y);
+                        if (i == 1)
+                            image(imgLoad.dueShitting, p[i].location.x, p[i].location.y);
                         p[i].iHaveShot = true;
                     }
                     if (!p[i].iHaveShot)
@@ -143,21 +145,23 @@ int var =p[0].levelTransition(pressed,oatList);
                 for (int i = 0; i < oatList.size(); i++) {
                     oatList.get(i).moveOatmeal();
                     oatList.get(i).drawOatmeal();
-                    oatList.get(i).outOfBounds(oatList,i);
-                    for(int j = 0;j<enemyList.size();j++){
-                        oatList.get(i).hit(oatList,enemyList.get(j),i,uncleroger);
+                    oatList.get(i).outOfBounds(oatList, i);
+                    for (int j = 0; j < enemyList.size(); j++) {
+                        oatList.get(i).hit(oatList, enemyList.get(j), i, uncleroger);
 
 
                     }
 
                 }
+
+                //Enemy functions
                 for (int i = 0; i < enemyList.size(); i++) {
                     enemyList.get(i).shoot();
                     if (enemyList.get(i).iShootNow == true) {
                     }
                     enemyList.get(i).display(backgrounds.level);
                     enemyList.get(i).move();
-                    enemyList.get(i).imDead(enemyList,i);
+                    enemyList.get(i).imDead(enemyList, i);
                 }
 
                 for (int i = 0; i < dueAmount; i++) {
@@ -169,9 +173,10 @@ int var =p[0].levelTransition(pressed,oatList);
             }
         //text(frameRate,500,500);
         mPressed = false;
-            println(oatList.size());
+        println(oatList.size());
     }
 
+    //Key to initiate actions
     public void keyPressed() {
         pressed = true;
         if (p[0] != null)
@@ -184,6 +189,7 @@ int var =p[0].levelTransition(pressed,oatList);
             }
     }
 
+    //Keyreleased to stop initiated actions
     public void keyReleased() {
         pressed = false;
         if (p[0] != null)
@@ -195,6 +201,7 @@ int var =p[0].levelTransition(pressed,oatList);
             }
     }
 
+    //Initiator for buttons pressed
     public void mousePressed() {
         mPressed = true;
     }
